5f0dbe5dcc62edfc53bb83b1355056f985f674cd
Matthew Dean <matthew.dean@digicatapult.org.uk>
2022-05-16 13:28:14 +0100

    Add in OpenAPI spec for part update action

---
+++ app/api-v1/routes/part/{id}/metadata-update/{updateId}.js
@@ -0,0 +1,70 @@
+// eslint-disable-next-line no-unused-vars
+module.exports = function (partService) {
+  const doc = {
+    GET: async function (req, res) {
+      res.status(500).json({ message: 'Not Implemented' })
+    },
+  }
+
+  doc.GET.apiDoc = {
+    summary: 'Get Part Metadata Update Action',
+    parameters: [
+      {
+        description: 'Id of the part',
+        in: 'path',
+        required: true,
+        name: 'id',
+        allowEmptyValue: false,
+        schema: {
+          $ref: '#/components/schemas/ObjectReference',
+        },
+      },
+      {
+        description: 'Id of the Part Metadata Update action',
+        in: 'path',
+        required: true,
+        name: 'updateId',
+        allowEmptyValue: false,
+        schema: {
+          $ref: '#/components/schemas/ObjectReference',
+        },
+      },
+    ],
+    responses: {
+      200: {
+        description: 'Return Part Metadata Update Action',
+        content: {
+          'application/json': {
+            schema: {
+              $ref: '#/components/schemas/PartMetadataUpdate',
+            },
+          },
+        },
+      },
+      404: {
+        description: 'Part Metadata Update not found',
+        content: {
+          'application/json': {
+            schema: {
+              $ref: '#/components/responses/NotFoundError',
+            },
+          },
+        },
+      },
+      default: {
+        description: 'An error occurred',
+        content: {
+          'application/json': {
+            schema: {
+              $ref: '#/components/responses/Error',
+            },
+          },
+        },
+      },
+    },
+    security: [{ bearerAuth: [] }],
+    tags: ['part'],
+  }
+
+  return doc
+}
