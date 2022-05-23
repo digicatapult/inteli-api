42c306b3a8ec7754e922cf9aa2efcb2bfc97dcb7
Paulius <paulius.michelevicius@gmail.com>
2022-05-23 17:57:49 +0100

    IN-231: migrations and fixutres.

---
+++ app/api-v1/controllers/Recipe/__tests__/transaction_fixtures.js
@@ -0,0 +1,31 @@
+module.exports = {
+  recipeId: 'RECIPE00-0000-1000-8000-000000000000',
+  transactionsExample: [
+    {
+      id: 'TRASACTI-0000-1000-8000-000000000000',
+      recipe_id: 'RECIPE00-0000-1000-8000-000000000000',
+      token_id: 'token-id',
+      status: 'InBlock',
+      created_at: '2022-05-23T11:04:29.316Z',
+    },
+    {
+      id: 'TRASACTI-0000-2000-8000-000000000000',
+      recipe_id: 'RECIPE00-0000-1000-8000-00000000000',
+      token_id: 'token-id',
+      status: 'InBlock',
+      created_at: '2022-05-23T13:04:29.316Z',
+    },
+  ],
+  listResponse: [
+    {
+      id: 'TRASACTI-0000-1000-8000-000000000000',
+      status: 'InBlock',
+      submittedAt: '2022-05-23T11:04:29.316Z',
+    },
+    {
+      id: 'TRASACTI-0000-2000-8000-000000000000',
+      status: 'InBlock',
+      submittedAt: '2022-05-23T13:04:29.316Z',
+    },
+  ],
+}
