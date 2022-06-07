19edf9526efe954a29963f4273cd3e4d6ac2b402
n3op2 <paulius.michelevicius@gmail.com>
2022-06-07 23:42:27 +0100

    IN-233: create order transaction controller - before token image.

--- app/api-v1/controllers/Order/index.js
+++ app/api-v1/controllers/Order/index.js
@@ -1,3 +1,7 @@
+const { runProcess } = require('../../../utils/dscp-api')
+const db = require('../../../db')
+const { BadRequestError, NotFoundError } = require('../../../utils/errors')
+
 module.exports = {
   getAll: async function () {
     return { status: 500, response: { message: 'Not Implemented' } }
@@ -12,8 +16,28 @@ module.exports = {
     get: async () => {
       return { status: 500, response: { message: 'Not Implemented' } }
     },
-    create: async () => {
-      return { status: 500, response: { message: 'Not Implemented' } }
+    create: async (req) => {
+      const { id } = req.params
+      if (!id) throw new BadRequestError('missing params')
+
+      const [order] = await db.getOrder(id)
+      if (!order) throw new NotFoundError('order')
+
+      const payload = {
+        inputs: [],
+        outputs: [{
+          roles: { Owner: 'abc' },
+          metadata: {},
+        }]
+      }
+
+      runProcess(payload, req.token)
+      const transaction = await db.insertOrderTransaction(id)
+
+      return {
+        status: 201,
+        ...transaction,
+      }
     },
   },
 }
