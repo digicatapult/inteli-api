7eb6477c53141fa675bda802cf9329bf5924198b
n3op2 <paulius.michelevicius@gmail.com>
2022-06-21 16:50:36 +0100

    BUGFIX: updating seed function.

--- test/seeds/orders.js
+++ test/seeds/orders.js
@@ -19,13 +19,14 @@ const seed = async () => {
 
   await client('recipes').insert([
     {
-      id: '10000000-0000-1000-8000-000000000000',
-      external_id: 'foobar3000',
-      name: 'foobar3000',
+      id: '36345f4f-0000-42e2-83f9-79e2e195e000',
+      external_id: '045240',
+      name: 'Magical Part 1',
       image_attachment_id: '00000000-0000-1000-8000-000000000000',
-      material: 'foobar3000',
-      alloy: 'foobar3000',
-      price: 'foobar3000',
+      material: 'material',
+      alloy: 'alloy',
+      latest_token_id: 10,
+      price: '999.66',
       required_certs: JSON.stringify([{ description: 'foobar3000' }]),
       supplier: '5FHneW46xGXgs5mUiveU4sbTyGBzmstUspZC92UhjJM694ty',
       owner: '5GNJqTPyNqANBkUVMN1LPPrxXnFouWXoe2wNSmmEoLctxiZY',
@@ -34,16 +35,41 @@ const seed = async () => {
 
   await client('recipes').insert([
     {
-      id: '10000000-0000-1000-9000-000000000000',
+      id: '36345f4f-0000-42e2-83f9-79e2e195e001',
       external_id: 'supplier3000',
-      name: 'supplier3000',
+      name: 'Mystical Part 1',
       image_attachment_id: '00000000-0000-1000-8000-000000000000',
-      material: 'supplier3000',
-      alloy: 'supplier3000',
-      price: 'supplier3000',
+      material: 'material',
+      alloy: 'alloy',
+      latest_token_id: null,
+      price: '1000',
       required_certs: JSON.stringify([{ description: 'supplier3000' }]),
       supplier: '5FHneW46xGXgs5mUiveU4sbTyGBzmstUspZC92UhjJM694ty',
       owner: '5GNJqTPyNqANBkUVMN1LPPrxXnFouWXoe2wNSmmEoLctxiZY',
+    },
+  ])
+
+  // with reciope that does not have a token_id
+  await client('orders').insert([
+    {
+      id: '36345f4f-6535-42e2-83f9-79e2e195e111',
+      supplier: '5FHneW46xGXgs5mUiveU4sbTyGBzmstUspZC92UhjJM694ty',
+      items: ['10000000-0000-1000-9000-000000000000'],
+      purchaser: '5GNJqTPyNqANBkUVMN1LPPrxXnFouWXoe2wNSmmEoLctxiZY',
+      status: 'Created',
+      required_by: '2022-10-21T11:45:46.919Z',
+    },
+  ])
+
+  // with reciope that has a token_id
+  await client('orders').insert([
+    {
+      id: '36345f4f-6535-42e2-83f9-79e2e195e112',
+      supplier: '5FHneW46xGXgs5mUiveU4sbTyGBzmstUspZC92UhjJM694ty',
+      items: ['10000000-0000-1000-8000-000000000000'],
+      purchaser: '5GNJqTPyNqANBkUVMN1LPPrxXnFouWXoe2wNSmmEoLctxiZY',
+      status: 'Created',
+      required_by: '2022-10-21T11:45:46.919Z',
     },
   ])
 }
