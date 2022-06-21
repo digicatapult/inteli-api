3b9643e59f6ceebf724a49ad44ae03a938f6c3e2
Mark Addis <nitro_charlie@live.com>
2022-06-21 13:49:20 +0100

    First Commit

--- app/api-v1/controllers/Recipe/__tests__/recipe.test.js
+++ app/api-v1/controllers/Recipe/__tests__/recipe.test.js
@@ -3,12 +3,21 @@ const { stub } = require('sinon')
 
 const db = require('../../../../db')
 const identity = require('../../../services/identityService')
-const { get } = require('..')
+const { get, getById } = require('..')
 const { recipeExample } = require('./transaction_fixtures')
+const { BadRequestError, NotFoundError } = require('../../../../utils/errors')
 
 const getRecipes = async () => {
   try {
     return await get()
+  } catch (err) {
+    return err
+  }
+}
+
+const getRecipeByID = async (req) => {
+  try {
+    return await getById(req)
   } catch (err) {
     return err
   }
@@ -20,6 +29,7 @@ describe('recipe controller', () => {
 
   beforeEach(async () => {
     stubs.getRecipes = stub(db, 'getRecipes').resolves([recipeExample])
+    stubs.getRecipeByIDdb = stub(db, 'getRecipeByIDdb').resolves([recipeExample])
     stubs.getMemberByAddress = stub(identity, 'getMemberByAddress')
       .onFirstCall()
       .returns({ alias: 'supplier-alias-test' })
@@ -31,6 +41,7 @@ describe('recipe controller', () => {
   afterEach(async () => {
     stubs.getRecipes.restore()
     stubs.getMemberByAddress.restore()
+    stubs.getRecipeByIDdb.restore()
   })
 
   describe('if getRecipe method fails', () => {
@@ -96,5 +107,82 @@ describe('recipe controller', () => {
         owner: 'owner-alias-test',
       },
     ])
+  })
+
+  describe('Recipe /GET BY ID', () => {
+    describe('if req.params.id is not provided', () => {
+      beforeEach(async () => {
+        response = await getRecipeByID({ params: {} })
+      })
+
+      it('throws a validation error', async () => {
+        expect(response).to.be.an.instanceOf(BadRequestError)
+        expect(response.message).to.be.equal('Bad Request: missing params')
+      })
+
+      it('does not perform any database calls', () => {
+        expect(stubs.getRecipeByIDdb.calledOnce).to.equal(false)
+      })
+    })
+
+    describe('if no recipes are found with a given ID', () => {
+      beforeEach(async () => {
+        stubs.getRecipeByIDdb.restore()
+        stubs.getMemberByAddress.restore()
+
+        stubs.getRecipeByIDdb = stub(db, 'getRecipeByIDdb').resolves([])
+        stubs.getMemberByAddress = stub(identity, 'getMemberByAddress')
+          .onFirstCall()
+          .returns({ alias: 'supplier-alias-test' })
+          .onSecondCall()
+          .returns({ alias: 'owner-alias-test' })
+        response = await getRecipeByID({ params: { id: '1000000-2000-7000-8000-000000000000' } })
+      })
+
+      it('throws an error', () => {
+        expect(response.code).to.be.equal(404)
+        expect(response).to.be.an.instanceOf(NotFoundError)
+        expect(response.message).to.be.equal('Not Found: Recipe Not Found')
+      })
+    })
+
+    describe('happy path - recipe', () => {
+      beforeEach(async () => {
+        stubs.getRecipeByIDdb.resolves([recipeExample])
+        stubs.getMemberByAddress.restore()
+        stubs.getMemberByAddress = stub(identity, 'getMemberByAddress')
+          .onFirstCall()
+          .returns({ alias: recipeExample.supplier })
+          .onSecondCall()
+          .returns({ alias: recipeExample.owner })
+        response = await getRecipeByID({ params: { id: recipeExample.id } })
+      })
+
+      afterEach(async () => {
+        stubs.getMemberByAddress.restore()
+      })
+
+      it('returns a recipe', () => {
+        const { status, response: body } = response
+        expect(status).to.be.equal(200)
+        expect(body).to.deep.equal({
+          id: '10000000-0000-1000-8000-0000000000000',
+          externalId: 'TEST-externalId',
+          name: 'TEST-name',
+          imageAttachmentId: '00000000-0000-1000-8000-000000000000',
+          material: 'TEST-material',
+          alloy: 'TEST-alloy',
+          price: '99.99',
+          requiredCerts: [
+            {
+              description: 'TEST-certificate',
+            },
+          ],
+
+          supplier: '5FHneW46xGXgs5mUiveU4sbTyGBzmstUspZC92UhjJM694ty',
+          owner: '5GNJqTPyNqANBkUVMN1LPPrxXnFouWXoe2wNSmmEoLctxiZY',
+        })
+      })
+    })
   })
 })
