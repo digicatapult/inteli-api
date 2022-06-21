ee16b771e5e9862bd69d800d1d4136bd1200558d
n3op2 <paulius.michelevicius@gmail.com>
2022-06-21 17:14:23 +0100

    BUGFIX: add old recipes for existng integration tests.

--- test/seeds/orders.js
+++ test/seeds/orders.js
@@ -19,14 +19,13 @@ const seed = async () => {
 
   await client('recipes').insert([
     {
-      id: '36345f4f-0000-42e2-83f9-79e2e195e000',
-      external_id: '045240',
-      name: 'Magical Part 1',
+      id: '10000000-0000-1000-8000-000000000000',
+      external_id: 'foobar3000',
+      name: 'foobar3000',
       image_attachment_id: '00000000-0000-1000-8000-000000000000',
-      material: 'material',
-      alloy: 'alloy',
-      latest_token_id: 10,
-      price: '999.66',
+      material: 'foobar3000',
+      alloy: 'foobar3000',
+      price: 'foobar3000',
       required_certs: JSON.stringify([{ description: 'foobar3000' }]),
       supplier: '5FHneW46xGXgs5mUiveU4sbTyGBzmstUspZC92UhjJM694ty',
       owner: '5GNJqTPyNqANBkUVMN1LPPrxXnFouWXoe2wNSmmEoLctxiZY',
@@ -35,15 +34,30 @@ const seed = async () => {
 
   await client('recipes').insert([
     {
-      id: '36345f4f-0000-42e2-83f9-79e2e195e001',
+      id: '10000000-0000-1000-9000-000000000000',
       external_id: 'supplier3000',
-      name: 'Mystical Part 1',
+      name: 'supplier3000',
+      image_attachment_id: '00000000-0000-1000-8000-000000000000',
+      material: 'supplier3000',
+      alloy: 'supplier3000',
+      price: 'supplier3000',
+      required_certs: JSON.stringify([{ description: 'supplier3000' }]),
+      supplier: '5FHneW46xGXgs5mUiveU4sbTyGBzmstUspZC92UhjJM694ty',
+      owner: '5GNJqTPyNqANBkUVMN1LPPrxXnFouWXoe2wNSmmEoLctxiZY',
+    },
+  ])
+
+  await client('recipes').insert([
+    {
+      id: '36345f4f-0000-42e2-83f9-79e2e195e000',
+      external_id: '045240',
+      name: 'Magical Part 1',
       image_attachment_id: '00000000-0000-1000-8000-000000000000',
       material: 'material',
       alloy: 'alloy',
-      latest_token_id: null,
-      price: '1000',
-      required_certs: JSON.stringify([{ description: 'supplier3000' }]),
+      latest_token_id: 1,
+      price: '999.66',
+      required_certs: JSON.stringify([{ description: 'foobar3000' }]),
       supplier: '5FHneW46xGXgs5mUiveU4sbTyGBzmstUspZC92UhjJM694ty',
       owner: '5GNJqTPyNqANBkUVMN1LPPrxXnFouWXoe2wNSmmEoLctxiZY',
     },
@@ -52,7 +66,7 @@ const seed = async () => {
   /* eslint-disable */
   await Promise.all(['Created', 'Submitted', 'Rejected', 'Accepted', 'Amended'].map((status, i) => client('orders').insert([{
       id: `36345f4f-6535-42e2-83f9-79e2e195e11${i}`,
-      supplier: '5FHneW46xGXgs5mUiveU4sbTyGBzmstUspZC92UhjJM694ty',
+      supplier: '36345f4f-0000-42e2-83f9-79e2e195e000',
       items: ['10000000-0000-1000-9000-000000000000'],
       purchaser: '5GNJqTPyNqANBkUVMN1LPPrxXnFouWXoe2wNSmmEoLctxiZY',
       status,
