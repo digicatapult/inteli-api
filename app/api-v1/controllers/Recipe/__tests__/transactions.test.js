0c336eb87a660c40ed0b91d64f5395aab8e2f1d5
n3op2 <paulius.michelevicius@gmail.com>
2022-06-22 10:40:34 +0100

    BUGFIX: unit test scenario naming.

--- app/api-v1/controllers/Recipe/__tests__/transactions.test.js
+++ app/api-v1/controllers/Recipe/__tests__/transactions.test.js
@@ -49,7 +49,7 @@ const getTransaction = async (req) => {
   }
 }
 
-describe('recipe transaction', () => {
+describe('recipe transactions controller', () => {
   let stubs = {}
   let response
   let runProcessBody
